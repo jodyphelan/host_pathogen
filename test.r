48ce4e12f8aa5b0726da4fe1df92b81b33200c0e
Jody Phelan <jody.phelan@lshtm.ac.uk>
2017-02-23 21:45:33 +0000

    commit

--- test.r
+++
@@ -1,9 +0,0 @@
-args = commandArgs(trailingOnly=TRUE)
-
-if (length(args)!=1) {
-  stop("At least one argument must be supplied (input file).n", call.=FALSE)
-} else if (length(args)==1) {
-  print(args)
-}
-
-
