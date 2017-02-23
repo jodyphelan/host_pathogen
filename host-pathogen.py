ac5d9fd2959d7db996f90aa6cec0367ef73bb344
Jody Phelan <jody.phelan@lshtm.ac.uk>
2017-02-23 22:55:12 +0000

    commit

--- host-pathogen.py
+++ host-pathogen.py
@@ -36,6 +36,6 @@ with open("xargs.txt","w") as o:
 
 subprocess.call("cat xargs.txt | xargs -n6 -P%s %s/worker.py" % (threads,scriptDir),shell=True)
 
-subprocess.call("cat `ls %s.temp*` | pigz -p 20 -c > %s.results.gz" %(outfile,outfile),shell=True)
+subprocess.call("cat `ls %s.temp*` > %s.results.gz" %(outfile,outfile),shell=True)
 subprocess.call("cat %s.combin* | awk '{sum += $1} END {print sum}' > %s.numtests.txt" % (outfile,outfile),shell=True)
 subprocess.call("rm %s.combin* %s.temp*" % (outfile,outfile),shell=True)
